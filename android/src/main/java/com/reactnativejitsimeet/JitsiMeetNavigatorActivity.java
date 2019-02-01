fee2c6677fea2b5ce459f95c368440a0cbad7ec7
Sébastien Krafft <skrafft@gmail.com>
2019-02-01 16:57:04 +0100

    Upgrade to jitsi meet 1.21.0 on android

--- android/src/main/java/com/reactnativejitsimeet/JitsiMeetNavigatorActivity.java
+++ android/src/main/java/com/reactnativejitsimeet/JitsiMeetNavigatorActivity.java
@@ -3,26 +3,44 @@ package com.reactnativejitsimeet;
 import java.util.Map;
 import java.util.HashMap;
 
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.modules.core.PermissionListener;
 
 import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.bridge.ReadableMap;
 
 import org.jitsi.meet.sdk.JitsiMeetView;
 import org.jitsi.meet.sdk.JitsiMeetViewListener;
+import org.jitsi.meet.sdk.ReactActivityLifecycleCallbacks;
 
 public class JitsiMeetNavigatorActivity extends AppCompatActivity implements JitsiMeetViewListener{
+
+    /**
+     * Instance of the {@link JitsiMeetView} which this activity will display.
+     */
     private JitsiMeetView view;
+
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+        ReactActivityLifecycleCallbacks.onActivityResult(
+                this, requestCode, resultCode, data);
+    }
 
     @Override
     public void onBackPressed() {
-        if (!JitsiMeetView.onBackPressed()) {
-            // Invoke the default handler if it wasn't handled by React.
-            super.onBackPressed();
-        }
+        ReactActivityLifecycleCallbacks.onBackPressed();
     }
 
     @Override
@@ -41,29 +59,47 @@ public class JitsiMeetNavigatorActivity extends AppCompatActivity implements Jit
     protected void onDestroy() {
         super.onDestroy();
 
-        view.dispose();
-        view = null;
+        if (view != null) {
+            view.dispose();
+            view = null;
+        }
 
-        JitsiMeetView.onHostDestroy(this);
+        ReactActivityLifecycleCallbacks.onHostDestroy(this);
     }
 
     @Override
     public void onNewIntent(Intent intent) {
-        JitsiMeetView.onNewIntent(intent);
+        ReactActivityLifecycleCallbacks.onNewIntent(intent);
+    }
+
+    // https://developer.android.com/reference/android/support/v4/app/ActivityCompat.OnRequestPermissionsResultCallback
+    @Override
+    public void onRequestPermissionsResult(
+            final int requestCode,
+            final String[] permissions,
+            final int[] grantResults) {
+        ReactActivityLifecycleCallbacks.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        JitsiMeetView.onHostResume(this);
+        ReactActivityLifecycleCallbacks.onHostResume(this);
     }
 
     @Override
-    protected void onStop() {
+    public void onStop() {
         super.onStop();
 
-        JitsiMeetView.onHostPause(this);
+        ReactActivityLifecycleCallbacks.onHostPause(this);
+    }
+
+    @Override
+    protected void onUserLeaveHint() {
+        if (view != null) {
+            view.enterPictureInPicture();
+        }
     }
 
     private void on(String name, Map<String, Object> data) {
