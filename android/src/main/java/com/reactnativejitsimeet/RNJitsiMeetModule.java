f0a3f4cab3ea53f463315115b1ca6c1a195e7a18
Muhammad-Wasi <wasi.ayub00@gmail.com>
2021-03-31 14:03:18 +0500

    Call option: update

--- android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
+++ android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
@@ -56,9 +56,14 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
                     RNJitsiMeetConferenceOptions options = new RNJitsiMeetConferenceOptions.Builder()
                             .setRoom(url)
                             .setAudioOnly(false)
-                            .setVideoMuted(true)
-                            .setting(false)
                             .setUserInfo(_userInfo)
+                            .setFeatureFlag("add-people.enabled", false)
+                            .setFeatureFlag("chat.enabled", false)
+                            .setFeatureFlag("live-streaming.enabled", false)
+                            .setFeatureFlag("meeting-name.enabled", false)
+                            .setFeatureFlag("meeting-password.enabled", false)
+                            .setFeatureFlag("recording.enabled", false)
+                            .setFeatureFlag("invite.enabled", false)
                             .build();
                     mJitsiMeetViewReference.getJitsiMeetView().join(options);
                 }
